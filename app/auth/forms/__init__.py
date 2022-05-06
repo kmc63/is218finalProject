938de03c07a87f5d1e8d8f9a684e9f16439d67b4
keith <keithcicala123@gmail.com>
2022-05-06 12:48:19 -0400

    profile form and user edit form added to auth forms

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -42,3 +42,15 @@ class create_user_form(FlaskForm):
     confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
     is_admin = BooleanField('Admin', render_kw={'value':'1'})
     submit = SubmitField()
+
+class profile_form(FlaskForm):
+    about = TextAreaField('About', [validators.length(min=6, max=300)],
+                          description="Please add information about yourself")
+
+    submit = SubmitField()
+
+class user_edit_form(FlaskForm):
+    about = TextAreaField('About', [validators.length(min=6, max=300)],
+                          description="Please add information about yourself")
+    is_admin = BooleanField('Admin', render_kw={'value':'1'})
+    submit = SubmitField()
