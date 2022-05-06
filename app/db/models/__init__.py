499147534407bfa8ec9e176d814cdbbfc47d73b1
keith <keithcicala123@gmail.com>
2022-05-06 14:12:06 -0400

    made changes to db model init and db init

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,8 +1,30 @@
 from datetime import datetime
 
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+Base = declarative_base()
+
+transaction_user = db.Table('transaction_user', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('transaction_id', db.Integer, db.ForeignKey('transaction.id'))
+)
+
+class transaction(db.Model,SerializerMixin):
+    __tablename__ = 'transaction'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(10), nullable=True, unique=False)
+    amount = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = relationship("User", back_populates="transaction", uselist=False)
+
+    def __init__(self, type, amount):
+        self.type = type
+        self.amount = amount
+
 
 
 class User(UserMixin, db.Model):
@@ -17,13 +39,17 @@ class User(UserMixin, db.Model):
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
     is_admin = db.Column('is_admin', db.Boolean(), nullable=False, server_default='0')
 
+    transaction = db.relationship("transaction",
+                            secondary=transaction_user, backref="users")
+
     # `roles` and `groups` are reserved words that *must* be defined
     # on the `User` model to use group- or role-based authorization.
 
-    def __init__(self, email, password):
+    def __init__(self, email, password, is_admin):
         self.email = email
         self.password = password
         self.registered_on = datetime.utcnow()
+        self.is_admin = is_admin
 
     def is_authenticated(self):
         return True
