8764365e7836486c6d84674b6fe4ac3faa7f32c5
keith <keithcicala123@gmail.com>
2022-05-05 23:58:20 -0400

    adding auth directory to setup new landing page

--- app/__init__.py
+++ app/__init__.py
@@ -11,24 +11,33 @@ from app.cli import create_database
 from app.db import db
 from app.db.models import User
 
-
-def page_not_found(e):
-    return render_template("404.html"), 404
+login_manager = flask_login.LoginManager()
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if os.environ.get("FLASK_ENV") == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif os.environ.get("FLASK_ENV") == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif os.environ.get("FLASK_ENV") == "testing":
+        app.config.from_object("app.config.TestingConfig")
+    #app.mail = Mail / (app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
 
     bootstrap = Bootstrap5(app)
     app.register_blueprint(simple_pages)
 
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-    # add command function to cli commands
     app.cli.add_command(create_database)
+    db.init_app(app)
 
     return app
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
---
+++ app/auth/__init__.py
@@ -0,0 +1,64 @@
+import logging
+ from flask import Blueprint, render_template, redirect, url_for, flash, current_app, abort
+from flask_login import login_user, login_required, logout_user, current_user
+from jinja2 import TemplateNotFound
+from sqlalchemy import select
+from werkzeug.security import generate_password_hash
+
+from app.auth.decorators import admin_required
+from app.auth.forms import login_form, register_form, profile_form, security_form, user_edit_form, create_user_form
+from app.db import db
+from app.db.models import User, Location, location_user
+from flask_mail import Message
+
+auth = Blueprint('auth', __name__, template_folder='templates')
+
+@auth.route('/register', methods=['POST', 'GET'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.dashboard'))
+    form = register_form()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = User(email=form.email.data, password=generate_password_hash(form.password.data), is_admin=0)
+            db.session.add(user)
+            db.session.commit()
+            if user.id == 1:
+                user.is_admin = 1
+                db.session.add(user)
+                db.session.commit()
+
+            msg = Message("Welcome to the site",
+                          sender="from@example.com",
+                          recipients=[user.email])
+            msg.body = "Welcome to the site"
+
+            current_app.mail.send(msg)
+            flash('Congratulations, you are now a registered user!', "success")
+
+            return redirect(url_for('auth.login'), 302)
+
+        else:
+            flash('Already Registered')
+            return redirect(url_for('auth.login'), 302)
+    return render_template('register.html', form=form)
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    form = login_form()
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.dashboard'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        else:
+            user.authenticated = True
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            flash("Welcome", 'success')
+            return redirect(url_for('auth.dashboard'))
+    return render_template('login.html', form=form)
