1995929aeb8663da2e444a52c9594ef1955159f0
keith <keithcicala123@gmail.com>
2022-05-05 23:27:30 -0400

    added simple pages and worked on init files

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 """A simple flask web app"""
 import os
+
+import flask_login
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+
+from app.simple_pages import simple_pages
 from app.cli import create_database
 from app.db import db
 from app.db.models import User
@@ -14,6 +20,9 @@ def create_app():
     app = Flask(__name__)
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
 
+    bootstrap = Bootstrap5(app)
+    app.register_blueprint(simple_pages)
+
     app.register_error_handler(404, page_not_found)
     db_dir = "database/db.sqlite"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
@@ -21,9 +30,5 @@ def create_app():
     db.init_app(app)
     # add command function to cli commands
     app.cli.add_command(create_database)
-
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
 
     return app
