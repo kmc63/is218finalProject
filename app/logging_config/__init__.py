1d552d9b6e5d78332048ca01167cc2b788b2d6c0
keith <keithcicala123@gmail.com>
2022-05-06 14:33:53 -0400

    added logging configuration for all the different logs we need to take

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -35,3 +35,98 @@ def setup_logs():
     if not os.path.exists(logdir):
         os.mkdir(logdir)
     logging.config.dictConfig(LOGGING_CONFIG)
+
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',  # Default is stderr
+        },
+        'file.handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'handler.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.myapp': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'myapp.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.request': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'request.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.errors': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'errors.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.sqlalchemy': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'sqlalchemy.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+        'file.handler.werkzeug': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR,'werkzeug.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['default','file.handler'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        '__main__': {  # if __name__ == '__main__'
+            'handlers': ['default','file.handler'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'werkzeug': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.werkzeug'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'sqlalchemy.engine': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.sqlalchemy'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'myApp': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.myapp'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'myerrors': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.errors'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+
+    }
+}
