1066a7ba289cfc34c02f3466b6e176a8a9d39983
keith <keithcicala123@gmail.com>
2022-05-11 21:42:11 -0400

    added test to calculate balance

--- README.md
+++ README.md
@@ -12,23 +12,23 @@
 Project Requirements
 
 1. Your project must have a log file with an entry for each time a user uploads a CSV account transaction file. 
-good
+100%
 2. There must be a test to verify that the CSV file is uploaded and processed
-0%
+100%
 3. You must create a database record that is related to the user record for each transaction
 50%
 4. You must calculate a balance and have a test for this
-0%
+100%
 5. You must have a test for login, a test for registration, a test for accessing the dashboard as a logged-in user, and a test for denying access to the dashboard, and denying access to uploading the CSV file
 100%
 6. You will need to have your app work with a dev, prod, and testing configuration file to control the behavior of the program in different environments.
-?
+100%
 
 
 You must
 
 7. You need at least 25 unit tests, each unit test is worth 4 points.  There must be at least one test for each thing that the user does in the app.
-13/25
+15/25
 8. You will need at least 25 commits (commit tests with their functional code in one commit)
 100%
 9. Have the correct badge on your readme for the development and production workflows respectively 
---
+++ tests/csv_balance_test.py
@@ -0,0 +1,41 @@
+import os
+from app import db
+from app.db.models import User, transactionDB
+from flask_login import FlaskLoginClient
+
+def test_csv_processed(application, client, add_user):
+    application.test_client_class = FlaskLoginClient
+    test_user = User.query.get(1)
+
+    with application.test_client(user=test_user) as client:
+        response = client.get('/transactions/upload')
+        assert response.status_code == 200
+
+        root = os.path.dirname(os.path.abspath(__file__))
+        test_csv_file = os.path.join(root, '../uploads/transactions2.csv')
+        test_csv_file_data = open(test_csv_file, 'rb')
+        test_data = {'file': (test_csv_file_data, 'transactions2.csv')}
+        response2 = client.post('/transactions/upload', data=test_data)
+
+        totalBalance = 0
+        userDebitBalance = 0
+        userCreditBalance = 0
+
+        assert totalBalance == 0
+        assert userDebitBalance == 0
+        assert userCreditBalance == 0
+
+        userDebit = transactionDB.query.filter_by(user_id=1, type='DEBIT')
+        for debit in userDebit:
+            userDebitBalance = userDebitBalance + int(debit.amount)
+
+        userCredit = transactionDB.query.filter_by(user_id=1, type='CREDIT')
+        for credit in userCredit:
+            userCreditBalance = userCreditBalance + int(credit.amount)
+
+        assert userDebitBalance == -27000
+        assert userCreditBalance == 25690
+
+        totalBalance = userDebitBalance + userCreditBalance
+
+        assert totalBalance == -1310
