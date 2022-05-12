d06b4be7fbb54eb445b3cf5dde372307a7f1c7d5
keith <keithcicala123@gmail.com>
2022-05-11 21:45:21 -0400

    fixed directory path issue since the path was a local path which made the py test fail

--- tests/csv_processed_test.py
+++ tests/csv_processed_test.py
@@ -11,7 +11,7 @@ def test_csv_processed(application, client, add_user):
         assert response.status_code == 200
 
         root = os.path.dirname(os.path.abspath(__file__))
-        test_csv_file = os.path.join(root, '../uploads/transactions2.csv')
+        test_csv_file = os.path.join(root, '../uploads2/transactions2.csv')
         test_csv_file_data = open(test_csv_file, 'rb')
         test_data = {'file': (test_csv_file_data, 'transactions2.csv')}
         response2 = client.post('/transactions/upload', data=test_data)
