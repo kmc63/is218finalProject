680053e9278054890ebf69cf35bee1fcdd060a6b
keith <keithcicala123@gmail.com>
2022-05-11 20:32:47 -0400

    register test for success and failure and spelling fixes

--- tests/user_input_test.py
+++ tests/user_input_test.py
@@ -3,17 +3,29 @@ from app.auth.forms import *
 from flask_login import FlaskLoginClient
 from app.db.models import User
 
-def test_login_sucess(application, client):
+def test_login_success(application, client):
     test_login = login_form()
     test_login.email.data = "keith@njit.edu"
     test_login.password.data = "password"
     assert test_login.validate()
 
-def test_login_unsucessful(application, client):
+def test_login_unsuccessful(application, client):
     test_login = login_form()
     test_login.email.data = "keith@njit.edu"
     test_login.password.data = "pass"
     assert not test_login.validate()
 
+def test_register_success(application, client):
+    test_register = register_form
+    test_register.email.data = "keith@njit.edu"
+    test_register.password.data = "password"
+    test_register.confirm.data = "password"
+    assert test_register.validate()
 
+def test_register_unsuccessfull(application, client):
+    test_register = register_form
+    test_register.email.data = "keith@njit.edu"
+    test_register.password.data = "password"
+    test_register.confirm.data = "password1"
+    assert not test_register.validate()
 
